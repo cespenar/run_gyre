cf795f681b73f8e18d7adb25f3a0c0f00c640123
cespenar <cespenar1@gmail.com>
2020-01-27 14:40:25 +0100

    added new parser options

--- run_gyre.py
+++ run_gyre.py
@@ -31,10 +31,19 @@ if __name__ == "__main__":
         help="The pattern for glob to select the models for calcualtions.")
     parser.add_argument("-o", "--out-suffix", type=str, default="_summary.txt", \
         help="The suffix of the gyre summary files.")
+    parser.add_argument("--nad", action="store_true", \
+        help="Process nonadiabatic output.")
+    parser.add_argument("--disable-ad", action="store_true", \
+        help="Do not process nonadiabatic output.")
     args = parser.parse_args()
 
     name_pattern = args.pattern
-    gyre_summary_ad = "summary.txt"
+
+    if not args.disable_ad:
+        gyre_summary_ad = "summary.txt"
+    
+    if args.nad:
+        gyre_summary_nad = "summary_nad.txt"
 
     gyre_env = "GYRE_DIR"
     if args.gyre_location:
@@ -73,14 +82,23 @@ if __name__ == "__main__":
                 print(f"Unexpected error: {sys.exc_info():s}")
                 exit(1)
 
-            gyre_result_ad = os.path.splitext(os.path.splitext(os.path.basename(model))[0])[0] \
-                + args.out_suffix
-            
-            try:
-                shutil.move(gyre_summary_ad, os.path.join(args.work_dir, gyre_result_ad))
-            except:
-                print(f"Unexpected error: {sys.exc_info():s}")
-                exit(1)
+            if not args.disable_ad:
+                gyre_result_ad = os.path.splitext(os.path.splitext(os.path.basename(model))[0])[0] \
+                    + args.out_suffix
+                try:
+                    shutil.move(gyre_summary_ad, os.path.join(args.work_dir, gyre_result_ad))
+                except:
+                    print(f"Unexpected error: {sys.exc_info():s}")
+                    exit(1)
+
+            if args.nad:
+                gyre_result_nad = os.path.splitext(os.path.splitext(os.path.basename(model))[0])[0] \
+                    + "_nad" + args.out_suffix
+                try:
+                    shutil.move(gyre_summary_nad, os.path.join(args.work_dir, gyre_result_nad))
+                except:
+                    print(f"Unexpected error: {sys.exc_info():s}")
+                    exit(1)
             
             print("Model calculated\n")
 
